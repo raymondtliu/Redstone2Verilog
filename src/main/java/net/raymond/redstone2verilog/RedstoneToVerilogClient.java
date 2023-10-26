b113f4702e54cb19259e93b914af39374d3513c9
Raymond Liu <raymond.t.liu@warwick.ac.uk>
2023-10-26 16:41:47 +0100

    initial commit

---
+++ src/main/java/net/raymond/redstone2verilog/RedstoneToVerilogClient.java
@@ -0,0 +1,10 @@
+package net.raymond.redstone2verilog;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RedstoneToVerilogClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/raymond/redstone2verilog/RedstoneToVerilogDataGenerator.java
@@ -0,0 +1,11 @@
+package net.raymond.redstone2verilog;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RedstoneToVerilogDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
