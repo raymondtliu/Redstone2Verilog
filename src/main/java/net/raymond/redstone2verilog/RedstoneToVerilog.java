86e36bef2f017c8ad3ca1cb6adaf1502ef4c44cd
Raymond Liu <raymond.t.liu@warwick.ac.uk>
2023-11-01 18:06:33 +0000

    added export item for debug purposes

--- src/main/java/net/raymond/redstone2verilog/RedstoneToVerilog.java
+++ src/main/java/net/raymond/redstone2verilog/RedstoneToVerilog.java
@@ -2,6 +2,7 @@ package net.raymond.redstone2verilog;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.raymond.redstone2verilog.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +16,6 @@ public class RedstoneToVerilog implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
